9e767ecbcd1fdd5398d1aaa804ab419b2fa337c4
Turk Erdin <ufkun@erdin.org>
2020-04-22 13:40:06 -0700

    Added new task view, no functionality

---
+++ TodoTasksApp/TaskViewController.swift
@@ -0,0 +1,22 @@
+//
+//  TaskViewController.swift
+//  TodoTasksApp
+//
+//  Created by Turk Erdin on 4/21/20.
+//  Copyright © 2020 Turk Erdin. All rights reserved.
+//
+
+import UIKit
+
+class TaskViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
--- TodoTasksApp/TodoViewController.swift
+++ TodoTasksApp/TodoViewController.swift
@@ -1,33 +1,69 @@
 //
-//  ViewController.swift
-//  TodoTasksApp
-//
 //  Created by Turk Erdin on 4/21/20.
 //  Copyright © 2020 Turk Erdin. All rights reserved.
 //
 
 import UIKit
+/**
+ * **class Task** - We use this to dictate how our individual tasks will behave. If we ever want to go back and add more parameters to tasks, it is easy to add new fields.
+ */
+class Task {
+    var name: String?
+    var description: String?
+    var complete: Bool?
+    
+    init(name: String, description: String, complete: Bool) {
+        self.name = name
+        self.description = description
+        self.complete = complete
+    }
+}
 
-class TodoViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+class TodoViewController: UIViewController {
     @IBOutlet weak var todoTableView: UITableView!
+    var tasks = [Task]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.title = "To-Do Tasks"
+        
+        todoTableView.delegate = self
+        todoTableView.dataSource = self
     }
     
-    // We just want the one section     UNNECESSARY?
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+    @IBAction func didTapAdd(_ sender: Any) {
+        performSegue(withIdentifier: "toNewTaskView", sender: self)
     }
+
+
+    func addNewTask(task: Task) {
+        tasks.append(task)
+    }
+
+}
+
+
+extension TodoViewController: UITableViewDelegate {
     
+}
+
+
+
+extension TodoViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+        tasks.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        let todoCell = tableView.dequeueReusableCell(withIdentifier: "TodoCell") ?? UITableViewCell()
+        
+        todoCell.textLabel?.text = tasks[indexPath.row].name
+        
+        return todoCell
+        
     }
-
-
+    
 }
 
